b1fefc25019ff4a5c511d45031dc6c5bbb986565
Reza-namvaran <reza.namvaran823@gmail.com>
2025-06-05 01:15:11 +0330

    (feat): Check for presence of security headers
    Add list of security headers to check
    
    Add HTTP GET request

--- main.go
+++ main.go
@@ -3,7 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"net/http"
+	"time"
 )
+
+var securityHeaders = []string{
+    "Content-Security-Policy",
+    "Strict-Transport-Security",
+    "X-Frame-Options",
+    "X-Content-Type-Options",
+    "X-XSS-Protection",
+    "Referrer-Policy",
+    "Permissions-Policy",
+}
+
 
 func main() {
 	if len(os.Args) < 2 {
@@ -12,5 +25,24 @@ func main() {
 	}
 
 	url := os.Args[1];
-	fmt.Printf("Analyzing: %s\n", url);
+	
+	client := &http.Client{Timeout: 5 * time.Second};
+	resp, err := client.Get(url);
+
+	if err != nil {
+		fmt.Printf("Failed to Fetech URL %v\n", err);
+		return;
+	}
+
+	defer resp.Body.Close();
+
+
+	fmt.Println("\nSecurity Header Check:")
+	for _, header := range securityHeaders {
+		if val := resp.Header.Get(header); val != "" {
+			fmt.Printf("✅ %s: Present\n", header)
+		} else {
+			fmt.Printf("❌ %s: Missing\n", header)
+		}
+	}
 }
